86593019110b192581838a0c643118b9cfba7719
mtmodesti <marcelomodesti1988@gmail.com>
2024-05-06 18:34:46 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {   
+        double salarioMinino = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 1;
+        numero = 2;
+        final double VALOR_PI = 3.14;
     }
 }
