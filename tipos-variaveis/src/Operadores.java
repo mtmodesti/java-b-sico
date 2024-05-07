807628e39c53dc51aa0b21beb565cdab1ba955a9
mtmodesti <marcelomodesti1988@gmail.com>
2024-05-07 18:20:08 -0300

    feat: operators

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main (String[] args){
+        String fullName = "Marcelo" + " Teixeira" + " Modesti";
+        String concatenacao = "?";
+        concatenacao = 1 + 1 + 1 + "1";
+        concatenacao = 1 + "1" + 1 + 1;
+        concatenacao = "1" + 1 + 1 + 1;
+        concatenacao = "1" +( 1 + 1 + 1);
+        System.out.print((fullName));
+        System.out.print((concatenacao));
+        
+    }
+}
